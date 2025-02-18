60f1f380e4f9f3f5cc13be6f66f1d9733743c488
Marko <markoandrijevic27@gmail.com>
2025-02-18 20:35:55 +0100

    dockerless file

--- vercel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    build: {
-      command: "prisma generate && next build", // Replace with your desired build command
-      output: "dist" // Replace with your output directory (if needed)
-    }
-  };
